d8f4d48c329ccc93b55c90312fb7cd376c4ce14e
mm <mmdevstuff@gmail.com>
2023-01-27 14:40:22 -0800

    Clean up

--- find.a.BLE/Model/AppDataManager.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import MVVM_D
-
-/// Application's Data Manager
-class AppDataManager: MVVMD_DataManager {
-    required init?() {
-        super.init(dataSources: [BLEDataSource.self])
-    }
-}
--- find.a.BLE/ViewModel/SettingsViewModel.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import Combine
-import SingleInstance
-
-class SettingsViewModel: SingleInstance, ObservableObject {
-    @Published var signalDisplayFormat: SignalDisplayFormat = .bars
-    @Published var timeHorizon: TimeHorizon = .forever
-}
